b5c5df847f1056d03b045572fd2fc1f5c8d473e6
Swastik2740S <onamverma8@gmail.com>
2025-07-22 10:45:57 +0530

    production frontend endpoint update

--- src/main/java/com/smarttask/security/SecurityConfig.java
+++ src/main/java/com/smarttask/security/SecurityConfig.java
@@ -49,12 +49,13 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // Make sure all frontend domains you use are here, https protocol included, no trailing slash!
-        configuration.setAllowedOrigins(Arrays.asList(
+        configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:3000",
                 "https://smart-front-one.vercel.app",
                 "https://smart-front-35tkuzfa1-swastik2740s-projects.vercel.app",
                 "https://smart-front-iota.vercel.app"
         ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"
